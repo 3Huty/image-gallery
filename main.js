7379227c8ea92e1057219b9d63ecf2b29b6e5e55
3Huty <sawczuk.a@gmail.com>
2018-04-18 10:11:43 +0200

    add main.js

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
+for (var i = 0; i<= 5; i++) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', images/ + i;);
   thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+      var srcImage = e.target.getAttribute ('img', 'src');
+        displayedImage(srcImage);
+    } 
+    }
+  function displayedImage(srcImage) {
+      
+  }
+    
+   
+}
 
 /* Wiring up the Darken/Lighten button */
 
