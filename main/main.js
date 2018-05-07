47683b6c0bce14e16b78c5f3bd26a23caa57c4d6
3Huty <sawczuk.a@gmail.com>
2018-05-08 00:09:25 +0200

    add style and main

---
+++ main/main.js
@@ -0,0 +1,36 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for (var i = 1; i<= 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  
+  newImage.onclick = function(e) {
+    var newImageUrl = e.target.getAttribute ('src');
+    return displayImage(newImageUrl)
+  } 
+}
+  
+function displayImage(newImageUrl) {
+  displayedImage.setAttribute('src', newImageUrl);
+}
+/* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+  var btnCheck = btn.getAttribute('class');
+  if(btnCheck === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+ }
